7c389d0c52e5f9e53433c0ab09d035891393e3ab
amaan-lari <amaanlari5@gmail.com>
2023-08-21 20:09:18 +0530

    Created a Datasource class in data package

---
+++ app/src/main/java/com/lari/affirmations/data/Datasource.kt
@@ -0,0 +1,22 @@
+package com.lari.affirmations.data
+
+import com.lari.affirmations.R
+import com.lari.affirmations.model.Affirmation
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
