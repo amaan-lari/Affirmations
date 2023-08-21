cebf01e37397878538473d1ee12038be95bb53f6
amaan-lari <amaanlari5@gmail.com>
2023-08-21 23:55:34 +0530

    Added comments for RecyclerView

--- app/src/main/java/com/lari/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/lari/affirmations/adapter/ItemAdapter.kt
@@ -9,14 +9,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.lari.affirmations.R
 import com.lari.affirmations.model.Affirmation
 
+/**
+ * Adapter for the [RecyclerView] in [com.lari.affirmations.MainActivity].
+ * Displays [Affirmation] data object.
+ */
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
     }
 
+    /**
+     * Create new views (invoked by the layout manager)
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item, parent, false)
@@ -24,10 +36,16 @@ class ItemAdapter(
         return ItemViewHolder(adapterLayout)
     }
 
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
     override fun getItemCount(): Int {
         return dataset.size
     }
 
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
