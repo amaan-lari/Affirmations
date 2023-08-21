1a5259a1b2925e36c01ff48fb89d12afc26bf81d
amaan-lari <amaanlari5@gmail.com>
2023-08-21 21:18:17 +0530

    Created an ItemAdapter class

---
+++ app/src/main/java/com/lari/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,11 @@
+package com.lari.affirmations.adapter
+
+import android.content.Context
+import com.lari.affirmations.model.Affirmation
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) {
+
+}
