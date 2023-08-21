10ca09c12534fd2351caf9869a3daf17a3c33fdc
amaan-lari <amaanlari5@gmail.com>
2023-08-21 21:22:27 +0530

    Created a ViewHolder

--- app/src/main/java/com/lari/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/lari/affirmations/adapter/ItemAdapter.kt
@@ -1,11 +1,17 @@
 package com.lari.affirmations.adapter
 
 import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.lari.affirmations.R
 import com.lari.affirmations.model.Affirmation
 
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Affirmation>
 ) {
-
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
 }
