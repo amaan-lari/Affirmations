28d697cf0f6f47bb422cc4d0142c82d3b270e32a
amaan-lari <amaanlari5@gmail.com>
2023-08-21 23:50:58 +0530

    Implemented onBindViewHolder()

--- app/src/main/java/com/lari/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/lari/affirmations/adapter/ItemAdapter.kt
@@ -29,6 +29,7 @@ class ItemAdapter(
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
     }
 }
